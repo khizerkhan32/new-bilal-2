8d4107d685ec3ea2cb3a7b52d8a1c4927bf6b13d
Khizer <degreatbunny@gmail.com>
2024-08-29 08:44:52 -0700

    2nd commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,13 +11,17 @@ const Hero = () => {
       <Header />
       <div className="h-screen w-full">
         <Container className="pt-24 flex flex-col-reverse md:flex-row justify-around md:justify-between items-center">
-          <div className="">
-            <h2>Hi There, I'm John Deo</h2>
-            <h1>Software Engineer</h1>
-            <p>based on United states</p>
-            <Button>Hire me</Button>
+          <div className="flex flex-col justify-between text-primary-500 text-center md:items-start gap-3 md:text-left">
+            <h2 className="text-2xl uppercase">Hi There, I'm John Deo</h2>
+            <h1 className="text-5xl md:text-7xl lg:whitespace-nowrap">
+              Software Engineer
+            </h1>
+            <p className="text-lg mb-6">based on United states</p>
+            <div>
+              <Button>Hire me</Button>
+            </div>
           </div>
-          <div className="">
+          <div className="w-[50vh] md:w-fit">
             <Lottie
               style={{ width: '100%', height: '100%' }}
               animationData={AnimationData}
