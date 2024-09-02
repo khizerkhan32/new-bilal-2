ac5b096e287907f9d72608be4e459407045df1ac
Khizer <degreatbunny@gmail.com>
2024-09-02 06:22:05 -0700

    first commit

--- src/components/StarsCanvas.jsx
+++ src/components/StarsCanvas.jsx
@@ -1,15 +1,12 @@
-import { useState, useRef, Suspense } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
+import { useState, useRef, Suspense, useEffect } from 'react';
+import { Canvas, useThree, useFrame } from '@react-three/fiber';
 import { Points, PointMaterial, Preload } from '@react-three/drei';
 import * as random from 'maath/random/dist/maath-random.esm';
 
 const Stars = (props) => {
   const ref = useRef();
-  const [sphere] = useState(
-    () =>
-      // random.inSphere(new Float32Array(5000), { radius: 1.2 })
-      random.inSphere(new Float32Array(5000 * 3), { radius: 1.2 })
-    // const positions = random.inSphere(new Float32Array(5000 * 3), { radius: 1.2 }
+  const [sphere] = useState(() =>
+    random.inSphere(new Float32Array(5000 * 3), { radius: 1.2 })
   );
 
   useFrame((state, delta) => {
@@ -35,11 +32,10 @@ const Stars = (props) => {
 const StarsCanvas = () => {
   return (
     <div className="w-full h-auto absolute inset-0 z-[0]">
-      <Canvas webgl1={true} camera={{ position: [0, 0, 1] }}>
+      <Canvas camera={{ position: [0, 0, 1] }}>
         <Suspense fallback={null}>
           <Stars />
         </Suspense>
-
         <Preload all />
       </Canvas>
     </div>
