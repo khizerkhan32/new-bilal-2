d50acff519f6d431551be6227cc31fd446a661bf
Khizer <degreatbunny@gmail.com>
2024-09-02 06:38:10 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ import Projects from './components/Projects';
 import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import StarsCanvas from './components/StarsCanvas';
+// import StarsCanvas from './components/StarsCanvas';
 
 const App = () => {
   return (
     <div className="relative overflow-x-hidden w-full backgroud">
-      <StarsCanvas />
+      {/* <StarsCanvas /> */}
 
       <div className="relative z-[1]">
         <Hero />
